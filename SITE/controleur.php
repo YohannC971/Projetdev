7757385dee81e9e416b318cbbfbacb3013d94421
YohannC971 <yohann.colletin@gmail.com>
2023-05-04 18:54:06 -0400

    push

---
+++ SITE/controleur.php
@@ -0,0 +1,29 @@
+<?php
+
+    include("model.php");
+    if(!isset($_POST))
+    {
+        include("index.php");
+        return;
+    }
+
+    if(!isset($_POST["Login"]) || !isset($_POST["Pass"]))
+    {
+        include("index.php");
+        return;
+    }
+
+    if (strlen($_POST["Login"])==0 || strlen($_POST["Pass"])==0) {
+        include("index.php");
+        return;
+    }
+
+    if (connexion($_POST["Login"],$_POST["Pass"])==1){
+        include("administration.php");
+    }
+    else 
+    {
+        include("index.php");
+    }
+
+?>
