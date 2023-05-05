5b5fbdbadad76f7e9d5fa8fc8ed1ffa153f7420e
YohannC971 <yohann.colletin@gmail.com>
2023-05-05 13:39:55 -0400

    model.php

--- model.php
+++ model.php
@@ -1,21 +1,26 @@
 <?php
 
 include("config.php");
-function connexion($l,$p)
-{
-    global $Login, $Pass;
 
-    if($l == $Login || $p == $Pass)
-    {
-        $_SESSION["etat"] = 1;
-        setcookie("etat","1",time()+60*5);
-        return 1;
-    }
-    else
-    {
-        $_SESSION["etat"] = 0;
-        return 0;
-    }
+function connexion($n,$p) {
+	global $HOST,$LOGINBDD,$PASSBDD,$BDD;
+	$c = new mysqli($HOST,$LOGINBDD,$PASSBDD,$BDD);
+	$r=$c->query("SELECT * FROM utilisateur WHERE `nom` = '$n' and `Pass`= '$p' ; ");
+	
+	if($r->num_rows==1) {
+		
+		$donnees= $r->fetch_array(MYSQLI_BOTH);
+		$_SESSION["nom"] = $donnees["nom"];
+		$_SESSION["prenom"] = $donnees["prenom"];
+	
+		setcookie("etat","1",time()+60*5);
+		$_SESSION["etat"]=1;
+		return 1;
+	} else {
+	$_SESSION["etat"]=0;
+	return 0;
+	}
+	
 }
 
 
