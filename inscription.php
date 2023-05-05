90502f516b3fdfeb4b1f429f60b387eaed70722a
YohannC971 <yohann.colletin@gmail.com>
2023-05-05 11:36:17 -0400

    connexion base de donnée

--- inscription.php
+++ inscription.php
@@ -1,33 +1,37 @@
 <?php
-// Informations de connexion à la base de données
-//$servername = "nom_du_serveur";
-//$username = "nom_utilisateur";
-//$password = "mot_de_passe";
-//$dbname = "nom_base_de_donnees";
 
-// Création de la connexion
-$conn = new mysqli($HOST, $LOGINBDD, $PASSBDD, $BDD);
+
+// Informations de connexion
+$servername = "localhost";
+$username = "user";
+$password = "root";
+$dbname = "prog_full_stack";
+
+// Connexion à la base de données
+$conn = mysqli_connect($servername, $username, $password, $dbname);
 
 // Vérification de la connexion
-if ($conn->connect_error) {
-  die("Connexion échouée: " . $conn->connect_error);
+if (!$conn) {
+    die("La connexion a échoué: " . mysqli_connect_error());
+    
 }
-
-// Récupération des données du formulaire d'inscription
+else{
+  // Récupération des données du formulaire d'inscription
 $nom = $_POST['nom'];
 $prenom = $_POST['prenom'];
 $email = $_POST['email'];
 
 // Requête SQL pour insérer un nouvel utilisateur dans la base de données
-$sql = "INSERT INTO Utilisateur (nom, prenom, email) VALUES ('" . $nom . "', '" . $prenom . "', '" . $email . "')";
+$sql = "INSERT INTO utilisateur (nom, prenom, email) VALUES ('" . $nom . "', '" . $prenom . "', '" . $email . "')";
 
 if ($conn->query($sql) === TRUE) {
   echo "Nouvel utilisateur créé avec succès";
+  header('Location: index.php');
 } else {
   echo "Erreur: " . $sql . "<br>" . $conn->error;
 }
-
-// Fermeture de la connexion à la base de données
-$conn->close();
+  echo "Connexion réussie";
+  header('Location: index.php');
+}
 
 ?>
