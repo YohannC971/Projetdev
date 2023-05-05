a5b1541516bb0654c57213fae54ce0d24789d6d7
YohannC971 <yohann.colletin@gmail.com>
2023-05-05 16:23:24 -0400

    Sécurisation fonction connexion

--- model.php
+++ model.php
@@ -2,26 +2,30 @@
 
 include("config.php");
 
-function connexion($n,$p) {
-	global $HOST,$LOGINBDD,$PASSBDD,$BDD;
-	$c = new mysqli($HOST,$LOGINBDD,$PASSBDD,$BDD);
-	$r=$c->query("SELECT * FROM utilisateur WHERE `nom` = '$n' and `Pass`= '$p' ; ");
-	
-	if($r->num_rows==1) {
-		
-		$donnees= $r->fetch_array(MYSQLI_BOTH);
-		$_SESSION["nom"] = $donnees["nom"];
-		$_SESSION["prenom"] = $donnees["prenom"];
-	
-		setcookie("etat","1",time()+60*5);
-		$_SESSION["etat"]=1;
-		return 1;
-	} else {
-	$_SESSION["etat"]=0;
-	return 0;
-	}
-	
+function connexion($n, $p) {
+    global $HOST, $LOGINBDD, $PASSBDD, $BDD;
+    $c = new mysqli($HOST, $LOGINBDD, $PASSBDD, $BDD);
+
+    // Utilisation d'une requête préparée avec des paramètres liés
+    $stmt = $c->prepare("SELECT * FROM utilisateur WHERE `nom` = ? AND `Pass`= ?");
+    $stmt->bind_param("ss", $n, $p);
+    $stmt->execute();
+    $r = $stmt->get_result();
+
+    if ($r->num_rows == 1) {
+        $donnees = $r->fetch_array(MYSQLI_BOTH);
+        $_SESSION["nom"] = $donnees["nom"];
+        $_SESSION["prenom"] = $donnees["prenom"];
+
+        setcookie("etat", "1", time() + 60 * 5);
+        $_SESSION["etat"] = 1;
+        return 1;
+    } else {
+        $_SESSION["etat"] = 0;
+        return 0;
+    }
 }
+
 
 
 
