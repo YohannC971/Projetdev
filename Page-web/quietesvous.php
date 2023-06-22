9247c85eb07476de8725496db66f6e748ef3e637
Enora-dev <e.appatore@hotmail.fr>
2023-06-22 14:50:59 -0400

    ff

--- Page-web/quietesvous.php
+++ Page-web/quietesvous.php
@@ -12,6 +12,7 @@ if (!isset($_SESSION)) {
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Formulaire de connexion</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" href="test.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="shortcut icon" type="image/png" href="./favicon/faviconmiage.png"/>
     <style>
         body {
             background-color: #fbfbfb;
