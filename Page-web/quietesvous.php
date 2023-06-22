dbc9a6b828f6d162d51f81d9085f5d83b9e454ec
Enora-dev <e.appatore@hotmail.fr>
2023-06-22 15:55:42 -0400

    ff

--- Page-web/quietesvous.php
+++ Page-web/quietesvous.php
@@ -28,10 +28,10 @@
         .Align {
             text-align: center;
             color: white;
-            font-family: Arial, Helvetica, sans-serif;
+            /*font-family: Arial, Helvetica, sans-serif;*/
         }
 
-        .B {
+        B {
             display: flex;
             justify-content: center;
             align-items: center;
@@ -40,6 +40,7 @@
             top: 50%;
             left: 50%;
             transform: translate(-50%, -50%);
+            border-radius: 30px; 
         }
 
         .C {
@@ -52,33 +53,54 @@
             justify-content: center;
             align-items: center;
             flex: 1;
+            border-radius: 30px; 
+        }
+
+        /* Style pour décaler le contenu */
+        .offset-content {
+            margin-top: 100px; /* Ajustez cette valeur selon vos besoins */
+            padding-top: 35px; /* Ajout du padding pour éviter le chevauchement */
+            position: relative;
+            z-index: 1; /* Positionne le contenu au-dessus de l'image de fond */
         }
     </style>
 </head>
 
 <body>
-    <div class="container text-center" style="margin-bottom: 20px;">
-        <img src="http://miage-antilles.fr/wp-content/uploads/2015/04/logo.png" style="max-width: 150px;" alt="logo">
-        <img src="http://miage-antilles.fr/wp-content/uploads/2021/11/GRETA-CFA-Gpe-Ptt.png" style="max-width: 300px;" alt="logo">
-    </div>
+    <!-- Navbar -->
+    <nav id="main-navbar" class="navbar navbar-expand-lg navbar-light bg-white fixed-top">
+        <!-- Container wrapper -->
+        <div class="container-fluid d-flex justify-content-center align-items-center">
+            <!-- Brand -->
+            <a class="navbar-brand d-flex align-items-center" href="#">
+                <img src="http://miage-antilles.fr/wp-content/uploads/2015/04/logo-300x245.png" style="margin-right: 20px;" height="80" alt="" loading="lazy" />
+                <img src="http://miage-antilles.fr/wp-content/uploads/2014/09/UA_logoCMJN_Corporate.jpg" style="margin-right: 20px;" height="80" alt="" loading="lazy">
+                <img src="http://miage-antilles.fr/wp-content/uploads/2021/11/GRETA-CFA-Gpe-Ptt.png" style="margin-right: 20px;" height="80" alt="" loading="lazy">
+            </a>
+        </div>
+        <!-- Container wrapper -->
+    </nav>
+    <!-- Navbar -->
 
     <div class="img"></div>
-    <div class="Align">
-        <h4>Bienvenue...</h4>
-    </div>
+    <div class="offset-content">
+        <div class="Align">
+            <h4>Nous vous souhaitons la bienvenue !</h4>
+        </div>
 
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-6 col-lg-4">
-                <div class="C">
-                    <h2>Etudiant</h2>
-                    <a href="index.php" class="btn btn-primary btn-lg active">Connexion</a>
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-6 col-lg-4">
+                    <div class="C">
+                        <h2>Etudiant</h2>
+                        <a href="index.php" class="btn btn-primary btn-lg active">Connexion</a>
+                    </div>
                 </div>
-            </div>
-            <div class="col-md-6 col-lg-4">
-                <div class="C">
-                    <h2>Professeur</h2>
-                    <a href="index.php" class="btn btn-primary btn-lg active">Connexion</a>
+                <div class="col-md-6 col-lg-4">
+                    <div class="C">
+                        <h2>Professeur</h2>
+                        <a href="index.php" class="btn btn-primary btn-lg active">Connexion</a>
+                    </div>
                 </div>
             </div>
         </div>
