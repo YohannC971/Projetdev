2759c30ee51dae2f7309db19534d159452ceafc1
YohannC971 <yohann.colletin@gmail.com>
2023-06-28 17:08:23 -0400

    tyh

--- Page-web/formulaire/controleur-responsable-legal.php
+++ Page-web/formulaire/controleur-responsable-legal.php
@@ -1,6 +1,6 @@
 <?php
-// Vérifier si l'utilisateur est connecté
 session_start();
+
 if (!isset($_SESSION['login'])) {
     header("Location: ../index.php");
     exit;
@@ -13,10 +13,8 @@ if ($conn->connect_error) {
     die("Erreur de connexion à la base de données : " . $conn->connect_error);
 }
 
-// Récupérer le login de l'utilisateur en session
 $login = $_SESSION['login'];
 
-// Requête SELECT pour récupérer l'ID de l'utilisateur
 $sql_select = "SELECT id_utilisateur FROM Utilisateur WHERE login_utilisateur='$login'";
 $result = $conn->query($sql_select);
 
@@ -24,43 +22,41 @@ if ($result->num_rows > 0) {
     $row = $result->fetch_assoc();
     $id_utilisateur = $row['id_utilisateur'];
 
-    // Récupérer l'intitulé de formation correspondant à id_formation_formation = 1
-    $sql_select_formation = "SELECT intitule_formation FROM Formation WHERE idformation_formation = 1";
-    $result_formation = $conn->query($sql_select_formation);
+        $nomparent1 = $_POST['nomparent1'];
+        $prenomparent1 = $_POST['prenomparent1'];
+        $numeroparent1 = $_POST['numeroparent1'];
+        $nomparent2 = $_POST['nomparent2'];
+        $prenomparent2 = $_POST['prenomparent2'];
+        $numeroparent2 = $_POST['numeroparent2'];
+        $nomresponsablelegal = $_POST['nomresponsablelegal'];
+        $prenomresponsablelegal = $_POST['prenomresponsablelegal'];
+        $numerorespnsablelegal = $_POST['numerorespnsablelegal'];
 
-    if ($result_formation->num_rows > 0) {
-        $row_formation = $result_formation->fetch_assoc();
-        $intitule_formation = $row_formation['intitule_formation'];
+        $sql_select_candidat_id = "SELECT idcandidat_candidat FROM Candidat WHERE id_utilisateur=$id_utilisateur";
+        $result_candidat_id = $conn->query($sql_select_candidat_id);
 
-        // Récupérer les autres valeurs à mettre à jour
-        $nom_jeune_fille = $_POST['nom_jeune_fille'];
-        $date_naissance = $_POST['date_naissance'];
-        $lieu_naissance = $_POST['lieu_naissance'];
-        $adresse = $_POST['adresse'];
-        $ville = $_POST['ville'];
-        $codePostal = $_POST['codePostal'];
-        $telephone = $_POST['telephone'];
-        $mobile = $_POST['mobile'];
+        if ($result_candidat_id->num_rows > 0) {
+            $row_candidat_id = $result_candidat_id->fetch_assoc();
+            $candidat_id = $row_candidat_id['idcandidat_candidat'];
 
-        // Mettre à jour les informations dans la table Candidat
-        $sql_update_candidat = "UPDATE Candidat SET nom_jeune_fille_candidat='$nom_jeune_fille', intitule_formation='$intitule_formation' WHERE id_utilisateur=$id_utilisateur";
-        if ($conn->query($sql_update_candidat) !== TRUE) {
-            echo "Erreur lors de la mise à jour du nom de Jeune Fille : " . $conn->error;
+            $sql_update_formulaire_id = "UPDATE Candidat SET formulaire_id_formulaire=(SELECT id_formulaire FROM Formulaire WHERE candidat_idcandidat_candidat=$candidat_id) WHERE id_utilisateur=$id_utilisateur";
+            if ($conn->query($sql_update_formulaire_id) !== TRUE) {
+                echo "Erreur lors de la mise à jour de formulaire_id_formulaire dans la table Candidat : " . $conn->error;
+                exit;
+            }
+
+            $sql_update_formulaire = "UPDATE Formulaire SET nom_parent1='$nomparent1', prennom_parent1='$prenomparent1', numero_parent1=$numeroparent1, nom_parent2=$nomparent2, prennom_parent2=$prenomparent2, 
+            numero_parent2=$numeroparent2, nom_responsable_legale=$nomresponsablelegal, prenom_responsable_legale=$prenomresponsablelegal, numero_responsable_legale=$numerorespnsablelegal  WHERE candidat_idcandidat_candidat=$candidat_id";
+            if ($conn->query($sql_update_formulaire) !== TRUE) {
+                echo "Erreur lors de la mise à jour des informations dans le formulaire : " . $conn->error;
+                exit;
+            }
+
+            header("Location: responsable_legal.html");
+        } else {
+            echo "Identifiant du candidat non trouvé.";
             exit;
         }
-
-        // Mettre à jour les informations dans la table Formulaire
-        $sql_update_formulaire = "UPDATE Formulaire SET datenaissance_formulaire='$date_naissance', ville='$ville', codepostal=$codePostal, telephone=$telephone, mobile=$mobile WHERE candidat_idcandidat_candidat=(SELECT idcandidat_candidat FROM Candidat WHERE id_utilisateur=$id_utilisateur)";
-        if ($conn->query($sql_update_formulaire && $sql_update_candidat2) !== TRUE) {
-            echo "Erreur lors de la mise à jour des informations dans le formulaire : " . $conn->error;
-            exit;
-        }
-
-        header("Location: responsable_legal.html");
-    } else {
-        echo "Formation non trouvée.";
-        exit;
-    }
 } else {
     echo "Utilisateur non trouvé.";
     exit;
