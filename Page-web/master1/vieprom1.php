3aa2b957545c7ce795a633bab06c7f9545e61361
Keny16 <keny.ek16@gmail.com>
2023-07-06 20:59:47 -0400

    ee

--- Page-web/master1/vieprom1.php
+++ Page-web/master1/vieprom1.php
@@ -381,7 +381,7 @@ if ($result->num_rows > 0) {
 
 <script>
   function suivant() {
-    window.location.href = "piece_justficatifm1"; // Remplacez l'URL par celle de la page suivante
+    window.location.href = "piece_justficatifm1.php"; // Remplacez l'URL par celle de la page suivante
   }
   </script>
 
